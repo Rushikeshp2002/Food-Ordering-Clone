2dad6d12d72768eba1e39df8da390ca52b97b331
Rushikesh Patil <rushikeshp7558@gmail.com>
2023-09-03 10:14:47 +0530

    Third Commit

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -2,11 +2,11 @@
 
 const Shimmer = () =>{
     return(
-        <div className="restraunt-list">
+        <div className="restraunt-list w-full h-full flex m-auto justify-center flex-wrap my-10 mx-2 animate-pulse">
             {Array(10)
             .fill("")
             .map((e,i)=>
-                <div className="Shimmer-card" key={i}></div>
+                <div className="w-72 h-80 bg-gray-400 rounded-xl  m-10" key={i}>.</div>
             )}
         </div>
 
