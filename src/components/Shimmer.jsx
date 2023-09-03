13a9358bdb8f94378cbf1512a01db7fc53c60f61
Rushikesh Patil <rushikeshp7558@gmail.com>
2023-09-03 10:19:04 +0530

    Fourth Commit

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -2,14 +2,20 @@
 
 const Shimmer = () =>{
     return(
+       <>
+       <>
+       <h1 className="text-center text-xl my-5">Make sure to install Allow Cors Extension from Chrome store and turn the toggle on</h1>
+        </>
         <div className="restraunt-list w-full h-full flex m-auto justify-center flex-wrap my-10 mx-2 animate-pulse">
-            {Array(10)
-            .fill("")
-            .map((e,i)=>
-                <div className="w-72 h-80 bg-gray-400 rounded-xl  m-10" key={i}>.</div>
-            )}
-        </div>
+        
+        {Array(10)
+        .fill("")
+        .map((e,i)=>
+            <div className="w-72 h-80 bg-gray-400 rounded-xl  m-10" key={i}>.</div>
+        )}
+    </div>
 
+       </>
     )
 }
 
